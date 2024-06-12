d01491064c3bc199dffe1d82cca55c4386a4cf72
Gabriel Ramirez <gabriel.ramirez.p.live@gmail.com>
2024-06-12 16:17:58 -0600

    personalizacion de modelos admin

--- prueba/registros/admin.py
+++ prueba/registros/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Alumnos
 
 # Register your models here.
-admin.site.register(Alumnos)
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    
+
+admin.site.register(Alumnos, AdministrarModelo)
--- prueba/registros/models.py
+++ prueba/registros/models.py
@@ -2,9 +2,19 @@ from django.db import models
 
 # Create your models here.
 class Alumnos(models.Model):
-    matricula = models.CharField(max_length=12) #Define la estructura de nuestra tabla
+    matricula = models.CharField(max_length=12, verbose_name="Mat") #Define la estructura de nuestra tabla
     nombre = models.TextField() #texto corto
     carrera = models.TextField() #texto largo
     turno = models.CharField(max_length=10)
     created =models.DateTimeField(auto_now_add=True)
     updated =models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Alumno"
+        verbose_name_plural = "Alumnos"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.nombre
+    
+
+
